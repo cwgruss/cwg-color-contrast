27fd2b652a29fcfc6ebdfae01d34af7b612c8a0c
Carter Gruss <cwgruss@gmail.com>
2019-06-16 11:13:46 -0500

    feat(Palette): Begin work on Color Palette model
    A color palette model will be useful for hosting multiple colors; it will serve as a simple collection object to set/remove and manage colors.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Color } from './util/color/color.model';
+import { ColorPalette } from './util/color/palette.model';
 import { luminance, contrastRatio } from './util/color/wcag-color-contrast';
 
 const white = new Color('#FFF');
@@ -7,15 +8,32 @@ const red = new Color('#FF0000');
 const green = new Color('#00FF00');
 const blue = new Color('#0000FF');
 
-const colors: Color[] = [
-   white,
-   black,
-   red,
-   green,
-   blue
-];
+const colors: ColorPalette = new ColorPalette('random colors', 
+   {
+       key: 'white',
+       value: white
+   },
+   {
+       key: 'black',
+       value: black
+   },
+   {
+       key: 'red',
+       value: red
+   },
+   {
+       key: 'blue',
+       value: blue
+   },
+);
 
-console.log(`${white.toHex()} against ${black.toHex()}: ${contrastRatio(white, black)}`);
-console.log(`${white.toHex()} against ${red.toHex()}: ${contrastRatio(white, red)}`);
-console.log(`${blue.toHex()} against ${black.toHex()}: ${contrastRatio(blue, black)}`);
+console.log(colors._toJSON());
 
+colors.values().forEach(color1 => {
+    const foreground = color1;
+
+    colors.values().forEach(color2 => {
+        const background = color2;
+        console.log(`${foreground.toHex()} with ${background.toHex()} has a constrast of ${contrastRatio(foreground, background)}`);
+    })
+})
