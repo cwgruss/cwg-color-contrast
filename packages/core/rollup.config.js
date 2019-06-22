28a30a31d8fbffaf54e322800eb7c29c0486fa24
Carter Gruss <cwgruss@gmail.com>
2019-06-22 12:05:24 -0500

    Build(Core): Make packages into Monorepo
    The core package should be responsible only for basic functionality: i.e. determining whether a color is compliant or not. For the visual comparison, it makes sense to create a separate application entirely and pull in core as a dependency. To manage this dependency tree, I am opting to use Lerna to create a Monorepo style repository.

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -24,6 +24,8 @@ export default {
     ],
     plugins: [
         typescript({
+			clean: true,
+			cacheRoot: './.tmp/.rpt2_cache',
             typescript: require('typescript')
         }),
         terser(),
