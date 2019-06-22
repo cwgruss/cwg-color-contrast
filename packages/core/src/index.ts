0098d2f06f6b46ccaaf89c627f0744219cb8494b
Carter Gruss <cwgruss@gmail.com>
2019-06-22 12:05:24 -0500

    Build(Core): Make packages into Monorepo
    The core package should be responsible only for basic functionality: i.e. determining whether a color is compliant or not. For the visual comparison, it makes sense to create a separate application entirely and pull in core as a dependency. To manage this dependency tree, I am opting to use Lerna to create a Monorepo style repository.

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export { Color } from './util/color/color.model';
+export { ColorPalette } from './util/color/palette.model';
+export { luminance, contrastRatio } from './util/color/wcag-color-contrast';
+ 
+
+
+
