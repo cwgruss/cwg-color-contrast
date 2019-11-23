db2a47e3630d7d36ce4fcd1ba39a3927d0084232
Carter Gruss <cwgruss@gmail.com>
2019-11-23 17:29:17 -0600

    feat(Palette): Improve functionality of the color palette
    The Palette class should behave much like an array, with a few added functions specific to colors and color options.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,16 @@
+import { ColorGroup } from './palette/color-group.model';
+import { Color } from './color/color.model';
+
 export { Color, isHSLValue, isHexValue,isRGBAValue, isRGBValue } from './color/color.model';
 export { luminance, contrastRatio } from './color/wcag-color-contrast';
 
+const colorGroup = new ColorGroup([
+    new Color('#FFF'),
+    new Color('#333'),
+    new Color('#000')
+])
 
+for (const color of colorGroup) {
+    console.log((color as Color).toRGB());
+    
+}
